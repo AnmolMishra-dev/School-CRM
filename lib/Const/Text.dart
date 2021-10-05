0d3a8886ef7413bccd2add4cb9b8a8c444e93dd1
Anmol Mishra <tbsdev3@gmail.com>
2021-10-05 12:30:16 +0530

    Commit

---
+++ lib/Const/Text.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyText{
+  static const String Side_heading = "Tbs";
+  static const String heading = "Welcome TechnoBrains Business  School Management System";
+
+}
